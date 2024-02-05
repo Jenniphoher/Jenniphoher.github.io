df0329b9db837605993710e421efd4c1ea978109
Jennifer Her <129333321+Jenniphoher@users.noreply.github.com>
2024-02-05 13:50:19 -0600

    Add files via upload

---
+++ box.js
@@ -0,0 +1,55 @@
+ function slideReact() {
+    var info = document.getElementById("in");
+    var sch = document.getElementById("sc");
+    var wrk = document.getElementById("wr");
+    var ext = document.getElementById("ex");
+    info.style.visibility = "visible";
+    
+    if(info.style.visibility = "visible") {
+      sch.style.visibility = "hidden";
+      wrk.style.visibility = "hidden";
+      ext.style.visibility = "hidden";
+    }
+  }
+
+  function slideReact1() {
+    var info = document.getElementById("in");
+    var sch = document.getElementById("sc");
+    var wrk = document.getElementById("wr");
+    var ext = document.getElementById("ex");
+    sch.style.visibility = "visible";
+    
+    if(sch.style.visibility = "visible") {
+      info.style.visibility = "hidden";
+      wrk.style.visibility = "hidden";
+      ext.style.visibility = "hidden";
+    }
+  }
+
+  function slideReact2() {
+    var info = document.getElementById("in");
+    var sch = document.getElementById("sc");
+    var wrk = document.getElementById("wr");
+    var ext = document.getElementById("ex");
+    wrk.style.visibility = "visible";
+    
+    if(wrk.style.visibility = "visible") {
+      info.style.visibility = "hidden";
+      sch.style.visibility = "hidden";
+      ext.style.visibility = "hidden";
+    } 
+  }
+
+ function slideReact3() {
+   var info = document.getElementById("in");
+   var sch = document.getElementById("sc");
+   var wrk = document.getElementById("wr");
+   var ext = document.getElementById("ex");
+   ext.style.visibility = "visible";
+    
+    if(ext.style.visibility = "visible") {
+      info.style.visibility = "hidden";
+      sch.style.visibility = "hidden";
+      wrk.style.visibility = "hidden";
+    } 
+  }
